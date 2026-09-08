39e9f30348e8539b168f8b128bcee1a59a40ba3f
ladyreina9966 <raissamarquesfonseca@gmail.com>
2026-09-08 16:51:33 -0300

    atual

--- Assets/script/MovePlayer.cs
+++ Assets/script/MovePlayer.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovePlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float _posicaox;
+    [SerializeField] float _posicaoy;
+
+    [SerializeField] Vector2 _posicao;
+
+    [SerializeField] Rigidbody2D _rig2d;
+
     void Start()
     {
-        
+        _rig2d = GetComponent<Rigidbody2D>();
+        _rig2d.angularVelocity = 0;
     }
-
+    public void SetMove(InputAction.CallbackContext value)
+    {
+        _posicao = value.ReadValue<Vector2>();
+    }
     // Update is called once per frame
     void Update()
     {
