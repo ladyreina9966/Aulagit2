278956f6962eb97513afaf9b88d35ae54b3654f3
ladyreina9966 <raissamarquesfonseca@gmail.com>
2026-09-10 17:03:36 -0300

    10.09 ATUA

--- Assets/script/MovePlayer.cs
+++ Assets/script/MovePlayer.cs
@@ -3,8 +3,7 @@ using UnityEngine.InputSystem;
 
 public class MovePlayer : MonoBehaviour
 {
-    [SerializeField] float _posicaox;
-    [SerializeField] float _posicaoy;
+    [SerializeField] float _velocidadeX;
 
     [SerializeField] Vector2 _posicao;
 
@@ -19,9 +18,21 @@ public class MovePlayer : MonoBehaviour
     {
         _posicao = value.ReadValue<Vector2>();
     }
+    public void SetJump(InputAction.CallbackContext value)
+    {
+        _rig2d.AddForceY(100);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        _rig2d.linearVelocityX = _posicao.x* _velocidadeX;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            Debug.Log("ground");
+        }
+
     }
 }
